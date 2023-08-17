372edb0c3f420f02e6257bd41a125e02677c61ff
sarang-26 <65524630+sarang-26@users.noreply.github.com>
2023-08-17 10:20:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import requests
 from transformers import BartTokenizer, BartForConditionalGeneration
 import time
 import gdown
+from transformers import BartConfig
+
 
 def download_from_google_drive(gdrive_url, output_path):
     # Extract file id from the Google Drive link
@@ -19,11 +21,11 @@ def load_model_from_google_drive(gdrive_url, model_path='model.pth'):
     return model
 
 
-
+config = BartConfig()
 tokenizer = BartTokenizer.from_pretrained("facebook/bart-large")
 model_url = "https://drive.google.com/file/d/1-DvdMr0vIJKKB3Efqz-zrImQcpOuc5Nq/view?usp=drive_link"
 downloaded_model = load_model_from_google_drive(model_url)
-model = BartForConditionalGeneration.from_pretrained("facebook/bart-large")
+model = BartForConditionalGeneration(config)
 model.load_state_dict(torch.load(downloaded_model, map_location=torch.device('cpu')))
 model.eval()
 device = torch.device("cpu")

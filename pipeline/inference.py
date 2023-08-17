cc6fd0a0ae38542275fb355ae090ee5f6bd74669
sarang-26 <65524630+sarang-26@users.noreply.github.com>
2023-08-17 10:20:35 +0200

    Update inference.py

--- pipeline/inference.py
+++ pipeline/inference.py
@@ -15,10 +15,10 @@ def generate_graphq_ir(query, model, tokenizer, device):
     # Ensure the model is in evaluation mode
 
     # Tokenize the input query
-    inputs = tokenizer([query], return_tensors="pt", max_length=256, truncation=True).to(device)
+    inputs = tokenizer([query], return_tensors="pt", max_length=512, truncation=True).to(device)
 
     # Generate the output sequence
-    output_ids = model.generate(inputs["input_ids"], attention_mask=inputs["attention_mask"], max_length=256, num_beams=5, temperature=0.7)
+    output_ids = model.generate(inputs["input_ids"], attention_mask=inputs["attention_mask"], max_length=512, num_beams=5, temperature=0.7)
 
     # Decode the generated output
     decoded_output = tokenizer.decode(output_ids[0], skip_special_tokens=True)
